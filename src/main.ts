08fa67336e91fee361581ea0bff2714431d1d2b6
kastov <kastov@gog.sh>
2025-02-23 23:13:31 +0300

    refactor: update Vision module routes and global prefix configuration

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import morgan from 'morgan';
 import { XRAY_INTERNAL_API_PORT, XRAY_INTERNAL_FULL_PATH } from '@libs/contracts/constants';
 import { NotFoundExceptionFilter } from '@common/exception/not-found-exception.filter';
 import { isDevelopment } from '@common/utils/is-development';
-import { ROOT } from '@libs/contracts/api';
+import { REST_API, ROOT } from '@libs/contracts/api';
 
 import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
@@ -44,7 +44,7 @@ async function bootstrap(): Promise<void> {
     app.useGlobalFilters(new NotFoundExceptionFilter());
 
     app.setGlobalPrefix(ROOT, {
-        exclude: [XRAY_INTERNAL_FULL_PATH],
+        exclude: [XRAY_INTERNAL_FULL_PATH, REST_API.VISION.BLOCK_IP, REST_API.VISION.UNBLOCK_IP],
     });
 
     app.useGlobalPipes(new ZodValidationPipe());
@@ -58,11 +58,14 @@ async function bootstrap(): Promise<void> {
     const internalApp = express();
     internalApp.use(json({ limit: '1000mb' }));
 
-    internalApp.use(XRAY_INTERNAL_FULL_PATH, (req, res, next) => {
-        req.url = req.originalUrl;
+    internalApp.use(
+        [XRAY_INTERNAL_FULL_PATH, REST_API.VISION.BLOCK_IP, REST_API.VISION.UNBLOCK_IP],
+        (req, res, next) => {
+            req.url = req.originalUrl;
 
-        httpServer.handle(req, res, next);
-    });
+            httpServer.handle(req, res, next);
+        },
+    );
 
     internalApp.listen(XRAY_INTERNAL_API_PORT, '127.0.0.1');
 }

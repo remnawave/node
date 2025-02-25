433527a6ed487da22e1d65e54fa021faed25d0c5
kastov <kastov@gog.sh>
2025-02-25 21:01:02 +0300

    fix: logger

--- src/common/guards/jwt-guards/def-jwt-guard.ts
+++ src/common/guards/jwt-guards/def-jwt-guard.ts
@@ -2,16 +2,14 @@ import { ExecutionContext, Logger, UnauthorizedException } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport';
 
 export class JwtDefaultGuard extends AuthGuard('registeredUserJWT') {
-    constructor(private readonly logger: Logger) {
-        super();
-    }
+    private readonly logger = new Logger(JwtDefaultGuard.name);
 
     handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
         if (info instanceof Error || err || !user) {
             const response = context.switchToHttp().getResponse();
 
             this.logger.error(
-                `Incorrect SSL_CERT or JWT! Request dropped. URL: ${context.switchToHttp().getRequest().url}, IP: ${context.switchToHttp().getRequest().ip}}`,
+                `Incorrect SSL_CERT or JWT! Request dropped. URL: ${context.switchToHttp().getRequest().url}, IP: ${context.switchToHttp().getRequest().ip}`,
             );
 
             response.socket?.destroy();

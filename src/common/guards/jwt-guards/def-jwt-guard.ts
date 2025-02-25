a8dfeb82a81627cc79fc036451888cfcacb4b367
kastov <kastov@gog.sh>
2025-02-25 20:47:51 +0300

    feat: enhance JWT authentication error logging
    - Add logger to JwtDefaultGuard to log authentication errors
    - Log specific error details when SSL certificate or JWT validation fails

--- src/common/guards/jwt-guards/def-jwt-guard.ts
+++ src/common/guards/jwt-guards/def-jwt-guard.ts
@@ -1,10 +1,17 @@
-import { ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, Logger, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 export class JwtDefaultGuard extends AuthGuard('registeredUserJWT') {
+    constructor(private readonly logger: Logger) {
+        super();
+    }
+
     handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
         if (info instanceof Error || err || !user) {
             const response = context.switchToHttp().getResponse();
+
+            this.logger.error(`Incorrent SSL_CERT or JWT: ${err}`);
+
             response.socket?.destroy();
             throw new UnauthorizedException('Unauthorized');
         }

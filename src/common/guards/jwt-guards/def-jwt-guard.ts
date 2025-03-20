963943d7fdb317e2e3da56c248275731fc245b52
kastov <kastov@gog.sh>
2025-03-20 17:11:04 +0300

    fix: log request headers for debugging in JwtDefaultGuard
    - Added logging of request headers in JwtDefaultGuard to assist with debugging SSL_CERT and JWT issues.
    - Marked the logging line with a TODO for future removal.

--- src/common/guards/jwt-guards/def-jwt-guard.ts
+++ src/common/guards/jwt-guards/def-jwt-guard.ts
@@ -8,6 +8,9 @@ export class JwtDefaultGuard extends AuthGuard('registeredUserJWT') {
         if (info instanceof Error || err || !user) {
             const response = context.switchToHttp().getResponse();
 
+            // TODO: remove later
+            this.logger.log(`${context.switchToHttp().getRequest().headers}`);
+
             this.logger.error(
                 `Incorrect SSL_CERT or JWT! Request dropped. URL: ${context.switchToHttp().getRequest().url}, IP: ${context.switchToHttp().getRequest().ip}`,
             );

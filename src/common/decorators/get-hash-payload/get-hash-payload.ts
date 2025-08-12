9b800cb6052cc4a376e776f804aa3485f229a0ad
kastov <kastov@gog.sh>
2025-08-12 19:48:00 +0300

    fix: improve hash payload handling in get-hash-payload decorator

--- src/common/decorators/get-hash-payload/get-hash-payload.ts
+++ src/common/decorators/get-hash-payload/get-hash-payload.ts
@@ -5,10 +5,11 @@ import { IHashPayload, X_HASH_PAYLOAD } from '@libs/contracts/constants';
 export const HashPayload = createParamDecorator((_, ctx: ExecutionContext): IHashPayload | null => {
     const request = ctx.switchToHttp().getRequest();
 
-    if (request.headers[X_HASH_PAYLOAD.toLowerCase()]) {
+    const hashPayload = request.headers[X_HASH_PAYLOAD.toLowerCase()];
+
+    if (hashPayload) {
         try {
-            const hashPayload = request.headers[X_HASH_PAYLOAD.toLowerCase()] as string;
-            const decodedPayload = Buffer.from(hashPayload, 'base64').toString('utf-8');
+            const decodedPayload = Buffer.from(hashPayload as string, 'base64').toString('utf-8');
             const hashPayloadJson = JSON.parse(decodedPayload);
 
             return hashPayloadJson as IHashPayload;

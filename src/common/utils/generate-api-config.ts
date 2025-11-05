d83aa892125def5eb3ba35dcad7bc182ae170d1d
kastov <kastov@gog.sh>
2025-11-05 06:02:40 +0300

    chore: disably policy overrides

--- src/common/utils/generate-api-config.ts
+++ src/common/utils/generate-api-config.ts
@@ -5,14 +5,29 @@ import {
     // XRAY_ROUTING_RULES_MODEL,
 } from '@libs/contracts/constants/xray';
 
+import { IPolicyConfig } from './interfaces';
+
 export const generateApiConfig = (config: Record<string, unknown>): Record<string, unknown> => {
     // const routing = config.routing as undefined | { rules?: unknown[] };
+    const policyConfig = config.policy as undefined | IPolicyConfig;
+
+    const builtPolicy: IPolicyConfig = {
+        levels: {
+            '0': {
+                ...(policyConfig?.levels?.['0'] || {}),
+                statsUserUplink: XRAY_DEFAULT_POLICY_MODEL.policy.levels['0'].statsUserUplink,
+                statsUserDownlink: XRAY_DEFAULT_POLICY_MODEL.policy.levels['0'].statsUserDownlink,
+                statsUserOnline: XRAY_DEFAULT_POLICY_MODEL.policy.levels['0'].statsUserOnline,
+            },
+        },
+        system: XRAY_DEFAULT_POLICY_MODEL.policy.system,
+    };
 
     return {
         ...config,
         ...XRAY_DEFAULT_STATS_MODEL,
-        ...XRAY_DEFAULT_POLICY_MODEL,
         ...XRAY_DEFAULT_API_MODEL,
+        policy: builtPolicy,
         // routing: {
         //     ...(routing || {}),
         //     rules: [XRAY_ROUTING_RULES_MODEL, ...(routing?.rules || [])],

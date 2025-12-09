17e1cd27c7bded21ffc8f18e71258ffbff00f9aa
kastov <kastov@gog.sh>
2025-12-10 01:57:23 +0300

    feat: add error handling for unsupported Remnawave versions in StartXrayCommand schema

--- libs/contract/commands/xray/start.command.ts
+++ libs/contract/commands/xray/start.command.ts
@@ -1,23 +1,40 @@
 import { z } from 'zod';
 
 import { REST_API } from '../../api';
+
+const UNSUPPORTED_REMNAWAVE_VERSION_MESSAGE = {
+    message:
+        'Unsupported Remnawave version. Please, upgrade Remnawave to version v2.3.x or higher. Or downgrade Remnawave Node version to v2.2.3 (change :latest to :2.2.3 in docker-compose.yml, most likely in /opt/remnanode)',
+};
+
 export namespace StartXrayCommand {
     export const url = REST_API.XRAY.START;
     export const RequestSchema = z.object({
-        internals: z.object({
-            forceRestart: z.boolean().default(false),
-            hashes: z.object({
-                emptyConfig: z.string(),
-                inbounds: z.array(
-                    z.object({
-                        usersCount: z.number(),
-                        hash: z.string(),
-                        tag: z.string(),
-                    }),
-                ),
+        internals: z.object(
+            {
+                forceRestart: z.boolean().default(false),
+                hashes: z.object({
+                    emptyConfig: z.string(),
+                    inbounds: z.array(
+                        z.object({
+                            usersCount: z.number(),
+                            hash: z.string(),
+                            tag: z.string(),
+                        }),
+                    ),
+                }),
+            },
+            {
+                errorMap: () => ({
+                    ...UNSUPPORTED_REMNAWAVE_VERSION_MESSAGE,
+                }),
+            },
+        ),
+        xrayConfig: z.record(z.unknown(), {
+            errorMap: () => ({
+                ...UNSUPPORTED_REMNAWAVE_VERSION_MESSAGE,
             }),
         }),
-        xrayConfig: z.record(z.unknown()),
     });
 
     export type Request = z.infer<typeof RequestSchema>;

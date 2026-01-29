405d04b2bb9154a9a72fab66804e7ac4ac4bdae8
kastov <kastov@gog.sh>
2026-01-29 03:05:26 +0300

    chore: update socket paths

--- libs/contract/constants/internal/internal.constants.ts
+++ libs/contract/constants/internal/internal.constants.ts
@@ -1,4 +1,4 @@
 export const XRAY_INTERNAL_API_CONTROLLER = 'internal';
 export const XRAY_INTERNAL_API_PATH = '/get-config';
 export const XRAY_INTERNAL_FULL_PATH = `/${XRAY_INTERNAL_API_CONTROLLER}${XRAY_INTERNAL_API_PATH}`;
-export const XRAY_INTERNAL_API_SOCKET_PATH = '/tmp/remnawave-internal.sock';
+export const XRAY_INTERNAL_API_SOCKET_PATH = '/run/rwnode/remnawave-internal.sock';

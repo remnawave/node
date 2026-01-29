27f3d5995b797fd31f3dd16159754f3981a00878
kastov <kastov@gog.sh>
2026-01-29 03:11:08 +0300

    fix: vars export

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,16 @@
 echo "[Entrypoint] Starting entrypoint script..."
 
 generate_random() {
-    tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32
+    tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 64
 }
 
 SUPERVISORD_USER=$(generate_random)
 SUPERVISORD_PASSWORD=$(generate_random)
 INTERNAL_REST_TOKEN=$(generate_random)
+
+export SUPERVISORD_USER
+export SUPERVISORD_PASSWORD
+export INTERNAL_REST_TOKEN
 
 echo "[Credentials] OK"
 
@@ -20,15 +24,12 @@ sleep 1
 echo "[Entrypoint] Getting Xray version..."
 
 XRAY_CORE_VERSION=$(/usr/local/bin/rw-core version | head -n 1)
+export XRAY_CORE_VERSION
 
 echo "[Entrypoint] Xray version: $XRAY_CORE_VERSION"
 echo "[Ports] XTLS_API_PORT: $XTLS_API_PORT"
 
 
-export SUPERVISORD_USER
-export SUPERVISORD_PASSWORD
-export INTERNAL_REST_TOKEN
-export XRAY_CORE_VERSION
 
 echo "[Entrypoint] Executing command: $@"
 exec "$@"

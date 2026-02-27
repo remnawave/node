2ff80b74e00e6f5c8ceceaa65af1fdaf5bba74aa
kastov <yk@sent.com>
2026-02-28 01:43:41 +0300

    fix: missing imports

--- src/modules/_plugin/plugin.module.ts
+++ src/modules/_plugin/plugin.module.ts
@@ -5,13 +5,14 @@ import { PluginStateService } from './services/plugin-state.service';
 import { PluginController } from './plugin.controller';
 import { NftService } from './services/nft.service';
 import { PluginService } from './plugin.service';
+import { COMMANDS } from './commands';
 import { QUERIES } from './queries';
 import { EVENTS } from './events';
 
 @Module({
     imports: [CqrsModule],
     controllers: [PluginController],
-    providers: [PluginService, PluginStateService, NftService, ...QUERIES, ...EVENTS],
+    providers: [PluginService, PluginStateService, NftService, ...QUERIES, ...EVENTS, ...COMMANDS],
     exports: [PluginStateService],
 })
 export class PluginModule {}
